3f6af5ae39903cbb55a12b7c2834f1f4661119d8
Neel Bhattacharya <neelbhatta22@gmail.com>
2024-05-13 12:13:36 +0530

    navbar, and some other components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 
 // here we will use class based components text_utils we use function based 
 import React, { Component } from 'react'
+import Navbar from './Components/Navbar';
+import News from './Components/News';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        hello my first class based Component
+        <Navbar/>
+        <News/>
       </div>
     )
   }
