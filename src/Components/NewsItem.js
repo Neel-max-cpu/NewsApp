17a694bf3adadff6ba3f188b4879c82a2e56ecca
Neel Bhattacharya <neelbhatta22@gmail.com>
2024-05-17 12:26:19 +0530

    using api to fetch

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -4,15 +4,18 @@ export class NewsItem extends Component {
 
   render() {
     let {title, description, imageUrl, url} = this.props
+      // default img to use if image is not preset -----
+      const defaultImageUrl = process.env.PUBLIC_URL + '/image/default.jpg';
+  
     return (
       <div className="my-3">
         <div className="card" style={{width: "18rem"}}>
-            <img src={imageUrl} className="card-img-top" alt="..."/>
+            <img src={imageUrl?imageUrl : defaultImageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}</p>
                 {/* target = "_blank" opens in a new tab */}
-                <a href={url} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+                <a rel="noreferrer" href={url} target="_blank" className="btn btn-sm btn-primary">Read More</a>
             </div>
         </div>
       </div>
